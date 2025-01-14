89728f090db4b132ef4b4fc22c4ed56dd0fe0a30
Akagi-Kazeno <akagikazeno@gmail.com>
2025-01-14 15:46:51 +0800

    feat(framework): optimize framework

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -2,6 +2,7 @@ import {defineStore} from "pinia";
 
 export const useAppStore = defineStore('AppStore', {
     state: () => ({}),
+    persist: true,
     getters: {},
     actions: {}
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
+import {fileURLToPath, URL} from "node:url";
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone.js"
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const version = dayjs().format('YYYYMMDDHHmmss');
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        vueJsx(),
+    ],
+    define: {
+        "import.meta.env.__VERSION__": JSON.stringify(version),
+    },
+    server: {
+        cors: true,
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].${version}.js`,
+                chunkFileNames: `assets/[name].${version}.js`,
+                assetFileNames: `assets/[name].${version}.[ext]`
+            }
+        }
+    }
 })
